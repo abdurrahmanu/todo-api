e8c2dc2587325550d3cf3f508824e422fe8c82fc
Ahmed Abdulrahman <ahamdabdulrahman103@gmail.com>
2024-01-04 17:48:42 +0100

    edit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -54,7 +54,6 @@ const updateTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({message: 'Task not found'})
         }
-
         res.status(200).json({task})
     } catch (error) {
         res.status(500).json({error: error})
